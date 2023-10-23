30d57e3702bfaed84420eafca368649f4ff0a949
Patrick Tanta <43278282+PatrickTanta@users.noreply.github.com>
2023-10-23 13:27:05 -0500

    fix: change docker compose depends on configuration

--- app/app/urls.py
+++ app/app/urls.py
@@ -1,9 +1,13 @@
-from django.contrib import admin
-from django.urls import path, include
 from drf_spectacular.views import (
     SpectacularAPIView,
     SpectacularSwaggerView,
 )
+
+from django.contrib import admin
+from django.urls import path, include
+from django.conf import settings
+
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -19,3 +23,10 @@ urlpatterns = [
     # api
     path('api/user/', include('user.urls'), name='user'),
 ]
+
+
+if settings.DEBUG:
+    urlpatterns += static(
+        settings.MEDIA_URL,
+        document_row = settings.MEDIA_ROOT
+    )
--- app/core/management/commands/wait_for_db.py
+++ app/core/management/commands/wait_for_db.py
@@ -16,12 +16,17 @@ class Command(BaseCommand):
         """Entrypoint for command"""
         self.stdout.write('Waiting for database...')
         db_up = False
+        max_retries = 0
 
         while db_up is False:
             try:
                 self.check(databases=['default'])
                 db_up = True
             except (Psycopg2OpError, OperationalError):
+                if max_retries > 10:
+                    return TimeoutError('Max retries exceeded')
+
                 self.stdout.write('Database unavailable, waiting 1 second...')
                 time.sleep(1)
+                max_retries += 1
         self.stdout.write(self.style.SUCCESS('Database available!'))
